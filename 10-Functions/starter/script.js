52c935792272788b508b73ccfec0411f0f8c9cb4
Kurt Carvey Cadenas <kurtcarvey.m.cadenas@gmail.com>
2024-08-06 00:35:11 +0800

    done: closures

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,299 @@
-'use strict';
+"use strict";
+
+/* const bookings = [];
+
+const createBooking = (
+  flightNum,
+  numPassengers = 1,
+  price = 199 * numPassengers,
+) => {
+  // ES5
+  // numPassengers = numPassengers || 1;
+  // price = price || 199;
+
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking("LH123");
+createBooking("LH123", 2, 800);
+createBooking("LH123", 2);
+createBooking("LH123", 5);
+
+createBooking("LH123", undefined, 1000); */
+
+/* const flight = "LH234";
+const jonas = {
+  name: "Jonas Schmedtmann",
+  passport: 24739479284,
+};
+
+const checkIn = (flightNum, passenger) => {
+  flightNum = "LH999";
+  passenger.name = `Mr. ${passenger.name}`;
+
+  if (passenger.passport === 24739479284) {
+    alert("Check in");
+  } else {
+    alert("Wrong passport!");
+  }
+};
+
+// checkIn(flight, jonas);
+// console.log(flight);
+// console.log(jonas);
+
+// Is the same as doing...
+// const flightNum = flight;
+// const passenger = jonas;
+
+const newPassport = (person) => {
+  person.passport = Math.trunc(Math.random() * 1000000000);
+};
+
+newPassport(jonas);
+checkIn(flight, jonas); */
+
+/* const oneWord = (str) => str.replace(/ /g, "").toLowerCase();
+
+const upperFirstWord = (str) => {
+  const [first, ...others] = str.split(" ");
+  return [first.toUpperCase(), ...others].join(" ");
+};
+
+const transformer = (str, fn) => {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer("JavaScript is the best!", upperFirstWord);
+transformer("JavaScript is the best!", oneWord);
+
+// JS uses callbacks all the time
+const high5 = () => {
+  console.log("👋");
+};
+
+document.body.addEventListener("click", high5);
+
+["Jonas", "Martha", "Adam"].forEach(high5); */
+
+/* const greet = (greeting) => (name) => console.log(`${greeting} ${name}`);
+
+const greeterHey = greet("Hey");
+greeterHey("Jonas");
+greeterHey("Steven");
+
+greet("Hello")("Jonas"); */
+
+/* const lufthansa = {
+  airline: 'Lufthasa',
+  iataCode: 'LH',
+  bookings: [],
+  book(flightNum, name) {
+    console.log(
+      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`,
+    );
+    this.bookings.push({
+      flight: `${this.iataCode}`,
+      name,
+    });
+  },
+};
+
+lufthansa.book(239, 'Kurt Carvey Cadenas');
+lufthansa.book(635, 'John Smith');
+console.log(lufthansa);
+
+const eurowings = {
+  airline: 'Eurowings',
+  iataCode: 'EW',
+  bookings: [],
+};
+
+const book = lufthansa.book;
+// Does NOT work
+// book(23, "Sarah Williams");
+
+book.call(eurowings, 23, 'Sarah Williams');
+console.log(eurowings);
+
+book.call(lufthansa, 239, 'Mary Cooper');
+console.log(lufthansa);
+
+const swiss = {
+  airline: 'Swiss Air Lines',
+  iataCode: 'LX',
+  bookings: [],
+};
+
+book.call(swiss, 583, 'Mary Cooper');
+console.log(swiss);
+
+// Apply method
+const flightData = [583, 'George Cooper'];
+book.apply(swiss, flightData);
+console.log(swiss);
+
+book.call(swiss, ...flightData);
+
+// Bind method
+// book.call(eurowings, 23, "Sarah Williams");
+
+const bookEW = book.bind(eurowings);
+const bookLH = book.bind(lufthansa);
+const bookLX = book.bind(swiss);
+
+bookEW(23, 'Steven Williams');
+
+const bookEW23 = book.bind(eurowings, 23);
+bookEW23('Carmelo Alexander Cadenas');
+bookEW23('Alixhan Cadenas');
+
+// With event listeners
+lufthansa.planes = 300;
+lufthansa.buyPlane = function () {
+  console.log(this);
+
+  this.planes++;
+  console.log(this.planes);
+};
+
+document
+  .querySelector('.buy')
+  .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+
+// Partial application
+
+const addTax = (rate, value) => value + value * rate;
+console.log(addTax(0.1, 200));
+
+const addVAT = addTax.bind(null, 0.23);
+// addTax = (rate, value) => value + value * 0.23;
+console.log(addVAT(100));
+console.log(addVAT(23));
+
+// Challenge
+const addTaxRate = rate => {
+  return value => value + value * rate;
+};
+
+const addVAT2 = addTaxRate(0.23);
+console.log(addVAT2(100));
+console.log(addVAT2(23)); */
+
+// Challenge #1
+/* 
+Let's build a simple poll app!
+A poll has a question, an array of options from which people can choose, and an array with the number of replies for each option. This data is stored in the starter 'poll' object below.
+
+Your tasks:
+1.	Create a method called 'registerNewAnswer' on the 'poll' object. The method does 2 things:
+1.1.	Display a prompt window for the user to input the number of the selected option. The prompt should look like this:
+What is your favourite programming language? 0: JavaScript
+1: Python
+2: Rust
+3: C++
+(Write option number)
+1.2.	Based on the input number, update the 'answers' array property. For example, if the option is 3, increase the value at position 3 of the array by
+1.	Make sure to check if the input is a number and if the number makes sense (e.g. answer 52 wouldn't make sense, right?)
+2.	Call this method whenever the user clicks the "Answer poll" button.
+3.	Create a method 'displayResults' which displays the poll results. The method takes a string as an input (called 'type'), which can be either 'string' or 'array'. If type is 'array', simply display the results array as it is, using console.log(). This should be the default option. If type is 'string', display a string like "Poll results are 13, 2, 4, 1".
+4.	Run the 'displayResults' method at the end of each
+'registerNewAnswer' method call.
+5.	Bonus: Use the 'displayResults' method to display the 2 arrays in the test data. Use both the 'array' and the 'string' option. Do not put the arrays in the poll object! So what should the this keyword look like in this situation?
+
+Test data for bonus:
+■	Data 1: [5, 2, 3]
+■	Data 2: [1, 5, 3, 9, 6, 1]
+
+Hints: Use many of the tools you learned about in this and the last section  
+
+GOOD LUCK  
+
+*/
+
+/* const poll = {
+  question: "What is your favourite programming language?",
+  options: ["0: JavaScript", "1: Python", "2: Rust", "3: C++"],
+  // This generates [0, 0, 0, 0]. More in the next section!
+  answers: new Array(4).fill(0),
+
+  registerNewAnswer() {
+    const answer = Number(
+      prompt(
+        `${this.question}\n${this.options.join("\n")}\n(Write option number)`,
+      ),
+    );
+
+    console.log(answer);
+
+    typeof answer === "number" &&
+      answer < this.answers.length &&
+      this.answers[answer]++;
+
+    this.displayResults();
+    this.displayResults("string");
+  },
+
+  displayResults(type = "array") {
+    if (type === "array") {
+      console.log(this.answers);
+    } else if (type === "string") {
+      console.log(`Poll results are ${this.answers.join(", ")}`);
+    }
+  },
+};
+
+// poll.registerNewAnswer();
+document
+  .querySelector(".poll")
+  .addEventListener("click", poll.registerNewAnswer.bind(poll));
+
+poll.displayResults.call({ answers: [5, 2, 3] }, "string");
+poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] }, "string");
+poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] });
+// ■	Data 1: [5, 2, 3]
+// ■	Data 2: [1, 5, 3, 9, 6, 1]
+*/
+
+/* const runOnce = () => {
+  console.log("This will never run again");
+};
+
+runOnce();
+
+// IIFE
+(() => {
+  console.log("This will never run again");
+})();
+
+{
+  const isPrivate = true;
+  var notPrivate = 46;
+}
+
+// console.log(isPrivate);
+console.log(notPrivate); */
+
+const secureBooking = () => {
+  let passengerCount = 0;
+  return () => {
+    passengerCount++;
+    console.log(`${passengerCount} passenger`);
+  };
+};
+
+const booker = secureBooking();
+
+booker();
+booker();
+booker();
